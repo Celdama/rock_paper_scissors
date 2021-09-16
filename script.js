bd41046b250f38ff8236fc397cb038b49b133647
Celdama <celdama.dev@gmail.com>
2021-09-16 16:55:05 +0200

    first version of the playRound function, it works but I am not ok with the number of if. The game works, I think about how to improve this code

--- script.js
+++ script.js
@@ -5,6 +5,41 @@ function computerPlay() {
   return choice[index];
 }
 
-const computerSelection = computerPlay();
+function playRound(playerSelection, computerSelection) {
+  let lowerPlayerSelection = playerSelection.toLowerCase();
+  let lowerComputerSelection = computerSelection.toLowerCase();
 
+  if (lowerPlayerSelection === lowerComputerSelection) {
+    return "It's a tied"
+  }
+
+  if (lowerPlayerSelection === 'rock') {
+    if (lowerComputerSelection === 'scissors') {
+      return "You win ! Rock beats Scissors";
+    } else {
+      return "You loose ! Papers beats Rock"
+    }
+  }
+
+  if (lowerPlayerSelection === 'papers') {
+    if (lowerComputerSelection === 'rock') {
+      return "You win ! Papers beats Rock";
+    } else {
+      return "You loose ! Scissors beats Papers"
+    }
+  }
+
+  if (lowerPlayerSelection === 'scissors') {
+    if (lowerComputerSelection === 'papers') {
+      return "You win ! Scissors beats papers";
+    } else {
+      return "You loose ! Rocks beats Scissors"
+    }
+  }
+}
+
+const playerSelection = "rock"
+const computerSelection = computerPlay();
 console.log(computerSelection)
+
+console.log(playRound(playerSelection, computerSelection))
