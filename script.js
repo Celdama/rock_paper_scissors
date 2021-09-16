ee52be70fdccdd88bfcf9172c3aa7d052a4b41f1
Celdama <celdama.dev@gmail.com>
2021-09-16 16:29:50 +0200

    create computerPlay that will randomly return Rock Paper or Scissors

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("Hello world");
+function computerPlay() {
+  const choice = ["Rock", "Paper", "Scissors"];
+  let index = Math.floor(Math.random() * choice.length);
+
+  return choice[index];
+}
+
+const computerSelection = computerPlay();
+
+console.log(computerSelection)
